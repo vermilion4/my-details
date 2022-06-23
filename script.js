67c2ae0c41b69a04b17cc99437a3ea6540972bb1
Adaeze Ndupu <andupu4@gmail.com>
2022-06-23 20:28:56 +0100

    Change to alert

--- script.js
+++ script.js
@@ -1,24 +1,18 @@
-// create object with my details
-let myDetails = {
-    name: "Adaeze Ndupu",
-    height: 1.95,
-    country: "Nigeria"
-}
+// assign details to the variables
+let myName = 'Adaeze Ndupu';
+let height = '1.65m';
+let country = 'Nigeria';
 
-// Create a p tag for the three properties of myDetails
-let name = document.createElement("p");
-let height = document.createElement("p");
-let country = document.createElement("p");
-
-// Add the text content to the p tags for name
-name.innerHTML = "Name: " + myDetails.name;
-// append the p tag to the div
-document.getElementById("demo").appendChild(name);
-
-// Add the text content to the p tags for height
-height.innerHTML = "Height: " + myDetails.height + "m";
-document.getElementById("demo").appendChild(height);
-
-// Add the text content to the p tags for country
-country.innerHTML = "Country: " + myDetails.country;
-document.getElementById("demo").appendChild(country);
+//alert the details
+alert(
+  'Name: ' +
+    myName +
+    '\n' +
+    'Height: ' +
+    height +
+    '\n' +
+    'Country: ' +
+    country +
+    '\n' +
+    ''
+);
